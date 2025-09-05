b85c9b98cbd1f495b85a7e374daaaa17e9e3789a
Gabriel <gabrielterciotti@gmail.com>
2025-09-04 22:58:48 -0300

    Adicionando testes em funçoes com listas

---
+++ src/gerenciarUsuarios.js
@@ -0,0 +1,16 @@
+const usuarios = ['matheus', 'marcos', 'lucas', 'joao'];
+
+function retornarUsuarios() {
+
+    return usuarios;
+}
+
+function adicionarNovoUsuario(nome) {
+    usuarios.push(nome);
+
+}
+
+module.exports = {
+    retornarUsuarios,
+    adicionarNovoUsuario
+}
