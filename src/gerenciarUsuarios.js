21bd37d58d142f94b3a17c813b04ffcb82124ad9
Gabriel <gabrielterciotti@gmail.com>
2025-09-09 22:58:25 -0300

    Mudei a lista de nomes de usuários para tornarem-se objetos

--- src/gerenciarUsuarios.js
+++ src/gerenciarUsuarios.js
@@ -1,12 +1,29 @@
-const usuarios = ['matheus', 'marcos', 'lucas', 'joao'];
+const usuarios = [
+    {
+        nome: 'Matheus',
+        email: 'matheus@gmail.com'
+     },
+    {
+        nome: 'Marcos',
+        email: 'marcos@gmail.com'
+    },
+    {
+        nome: 'Lucas',
+        email: 'lucas@gmail.com'
+    },
+    {
+        nome: 'João',
+        email: 'joao@gmail.com'
+    }
+];
 
 function retornarUsuarios() {
 
     return usuarios;
 }
 
-function adicionarNovoUsuario(nome) {
-    usuarios.push(nome);
+function adicionarNovoUsuario(usuario) {
+    usuarios.push(usuario);
 
 }
 
--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -4,13 +4,22 @@ const { expect } = require('chai');
 describe('Testar as funções de Gestão de Usuários', function () {
     it('Validar que posso adicionar um novo nome de usuário na lista', function () {
         //1. Adicionar um novo nome na lista de usuários
-        adicionarNovoUsuario('Maria');
+                adicionarNovoUsuario({
+                    nome: 'Maria',
+                    email: 'maria@gmail.com'
+                });
 
         //2. Retornar  a lista de usuários na caixa lista de usuários 
         const listaDeUsuarios = retornarUsuarios();
 
         //3. Comparar se o novo nome está no fim da lista de usuários
-        expect(listaDeUsuarios.at(-1)).to.equal('Maria');
+        //expect(listaDeUsuarios.at(-1).nome).to.equal('Maria');
+        //expect(listaDeUsuarios.at(-1).email).to.equal('maria@gmail.com');
+        expect(listaDeUsuarios.at(-1)).to.deep.equal({ 
+            //to.deep.equal ou to.eql devem ser usados quando eu comparo o objeto por inteiro
+            nome: 'Maria',
+            email: 'maria@gmail.com'
+        })
 
 
     });
